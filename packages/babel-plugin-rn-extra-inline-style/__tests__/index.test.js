a41d1727d5a27e9041b2892cd41a126f4bdfc268
hufeng <hufeng@qianmi.com>
2019-05-30 18:42:27 +0800

    添加对对象中的变量表达式的分离

--- packages/babel-plugin-rn-extra-inline-style/__tests__/index.test.js
+++ packages/babel-plugin-rn-extra-inline-style/__tests__/index.test.js
@@ -17,6 +17,22 @@ it('extra inline stle without import StyleSheet', () => {
   expect(code).toMatchSnapshot();
 });
 
+it('extra inline stle without import StyleSheet and with condition style', () => {
+  const tpl = `
+    const Hello = () => (
+      <View style={{flex: 1, backgroundColor: 'red', alignItems: isIOS ? "center" : "left"}}>
+        <Text style={styles.text}>hello world</Text>      
+      </View>
+    );
+  `;
+
+  const { code } = babel.transform(tpl, {
+    plugins: [extraInlineStyle, 'syntax-jsx']
+  });
+
+  expect(code).toMatchSnapshot();
+});
+
 it('extra inline style with import StyleSheet', () => {
   const tpl = `
   import {StyleSheet} from 'react-native';
@@ -43,6 +59,25 @@ it('extra inline style with import StyleSheet and multiple style', () => {
     <View style={{flex: 1, backgroundColor: 'red'}}>
       <Text style={styles.text}>hello world</Text>      
       <Text style={[styles.text, {color: 'yellow', fontSize: 12}]}>hello world</Text>      
+    </View>
+  );
+`;
+
+  const { code } = babel.transform(tpl, {
+    plugins: [[extraInlineStyle, { hash: 10 }], 'syntax-jsx']
+  });
+
+  expect(code).toMatchSnapshot();
+});
+
+it('extra inline style with import StyleSheet and multiple style and condition style', () => {
+  const tpl = `
+  import {StyleSheet} from 'react-native';
+
+  const Hello = () => (
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text style={styles.text}>hello world</Text>      
+      <Text style={[styles.text, {color: 'yellow', fontSize: 12, alignItems: isIOS ? "center" : "left"}]}>hello world</Text>      
     </View>
   );
 `;
--- packages/babel-plugin-rn-extra-inline-style/index.js
+++ packages/babel-plugin-rn-extra-inline-style/index.js
@@ -9,6 +9,10 @@ function md5(param, offset = 8) {
     .substring(0, offset);
 }
 
+/**
+ * 将样式的ast表达式还原成普通对象
+ * @param {*} objectProperties
+ */
 function extraObjectProperty(objectProperties) {
   return objectProperties.reduce((r, cur) => {
     const key = cur.key.name;
@@ -16,6 +20,34 @@ function extraObjectProperty(objectProperties) {
     r[key] = val;
     return r;
   }, {});
+}
+
+/**
+ * 分离出来那些属性值不是常量属性
+ * 返回不是常量属性的对象集合
+ *
+ * @param {*} t
+ * @param {*} expression
+ */
+function decomposeLiteralStyleProps(t, elem) {
+  const literal = [];
+  const other = [];
+  const objectProperties = elem.properties;
+
+  for (let prop of objectProperties) {
+    const value = prop.value;
+    if (t.isStringLiteral(value) || t.isNumericLiteral(value)) {
+      literal.push(prop);
+    } else {
+      other.push(prop);
+    }
+  }
+
+  // 修正当前的properties，只包含常量属性集合的属性
+  elem.properties = literal;
+
+  // 返回非常量属性的对象表达式
+  return other.length == 0 ? null : t.objectExpression(other);
 }
 
 module.exports = function(babel) {
@@ -36,17 +68,20 @@ module.exports = function(babel) {
             });
           }
 
+          // 将样式的数组合并成一个对象表达式
           const mapStyle = t.objectExpression(
             inlineStyle.map(({ styleName, val }) =>
               t.ObjectProperty(t.identifier(styleName), val)
             )
           );
 
+          // 使用StyleSheet.create去wrap当前的mapStyle
           const style = t.callExpression(
             t.memberExpression(importStyleSheet, t.identifier('create')),
             [mapStyle]
           );
 
+          // 添加变量
           path.pushContainer(
             'body',
             t.variableDeclaration('const', [
@@ -54,7 +89,7 @@ module.exports = function(babel) {
             ])
           );
 
-          // clean
+          // clean variable
           i = 0;
           inlineStyle = [];
           importStyleSheet = null;
@@ -84,6 +119,10 @@ module.exports = function(babel) {
 
         // 单个对象
         if (t.isObjectExpression(value.expression)) {
+          // 分解常量属性
+          const other = decomposeLiteralStyleProps(t, value.expression);
+
+          // 生成md5的stylename
           const rawStyle = extraObjectProperty(value.expression.properties);
           const styleName = '_' + md5(JSON.stringify(rawStyle), opts.hash);
 
@@ -92,15 +131,27 @@ module.exports = function(babel) {
             inlineStyle.push({ styleName, val: value.expression });
           }
 
-          value.expression = t.jSXMemberExpression(
-            t.jSXIdentifier('ai'),
-            t.jSXIdentifier(styleName)
-          );
+          if (other) {
+            value.expression = t.arrayExpression([
+              t.jSXMemberExpression(
+                t.jSXIdentifier('ai'),
+                t.jSXIdentifier(styleName)
+              ),
+              other
+            ]);
+          } else {
+            value.expression = t.jSXMemberExpression(
+              t.jSXIdentifier('ai'),
+              t.jSXIdentifier(styleName)
+            );
+          }
         }
         // 数组元素
         else if (t.isArrayExpression(value.expression)) {
-          value.expression.elements = value.expression.elements.map(elem => {
+          const elemes = [];
+          value.expression.elements.forEach(elem => {
             if (t.isObjectExpression(elem)) {
+              const other = decomposeLiteralStyleProps(t, elem);
               const rawStyle = extraObjectProperty(elem.properties);
               const styleName = '_' + md5(JSON.stringify(rawStyle), opts.hash);
 
@@ -109,14 +160,21 @@ module.exports = function(babel) {
                 inlineStyle.push({ styleName, val: elem });
               }
 
-              inlineStyle.push({ styleName, val: elem });
-              return t.jSXMemberExpression(
-                t.jSXIdentifier('ai'),
-                t.jSXIdentifier(styleName)
+              elemes.push(
+                t.jSXMemberExpression(
+                  t.jSXIdentifier('ai'),
+                  t.jSXIdentifier(styleName)
+                )
               );
+
+              if (other) {
+                elemes.push(other);
+              }
+            } else {
+              elemes.push(elem);
             }
-            return elem;
           });
+          value.expression.elements = elemes;
         }
       }
     }
